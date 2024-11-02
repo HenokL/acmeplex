9d66f1418e40348d2cb71298a132f669bdd9290a
Riley <koppangdev@gmail.com>
2024-11-02 13:15:31 -0600

    Setup boilerplate

---
+++ backend/src/main/java/com/movie_Theater/movie_ticket_reservation/MovieTicketReservationApplication.java
@@ -0,0 +1,13 @@
+package com.movie_Theater.movie_ticket_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketReservationApplication.class, args);
+	}
+
+}
